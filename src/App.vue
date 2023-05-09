3979fb795cba842e88817c3ed0af4fde84a46b56
Carlos Alexander Duarte Hernadez <64160994+CarlosDuarte1998@users.noreply.github.com>
2023-05-08 23:44:07 -0600

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import ComponentFooter from './components/ComponentFooter.vue';
+import ComponentHeader from './components/ComponentHeader.vue';
+import ComponentMain from './components/ComponentMain.vue';
+</script>
+
+<template>
+<ComponentFooter />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ComponentFooter.vue
@@ -0,0 +1,7 @@
+<template>
+<h1 class="p-5 text-3xl">Prueba</h1>
+</template>
+
+<style scoped>
+
+</style>
